1cbd202c05a8c3ff0decb3e85f99a0c8d06ceedd
Gregory-Georges <simon.schetagne@gmail.com>
2023-09-28 14:10:49 -0400

    Ajout du state machine

--- mastermind.py
+++ mastermind.py
@@ -26,7 +26,17 @@ class Mastermind_state:
 		pass
 		
 		
+
+# Stade jeu
+class Mastermind_jouer(Mastermind_state):
+	def __init__(self, data) :
+		Mastermind_state.__init__(self, data)
 		
+	def run(self) :
+		return self
+
+		
+				
 # Stade begin
 class Mastermind_begin(Mastermind_state):
 	
@@ -34,8 +44,37 @@ class Mastermind_begin(Mastermind_state):
 		Mastermind_state.__init__(self, data)
 		
 	def run(self) :
-		self.data.running = False
-		return Mastermind_begin(self.data)
+		self.afficher_intro() 
+		state = self.demander_continuer_quitter()
+		return state
+		
+	
+	
+	def afficher_intro(self) :
+		print(' __  __    __    ___  ____  ____  ____  __  __  ____  _  _  ____  ')
+		print('(  \/  )  /__\  / __)(_  _)( ___)(  _ \(  \/  )(_  _)( \( )(  _ \ ')
+		print(' )    (  /(__)\ \__ \  )(   )__)  )   / )    (  _)(_  )  (  )(_) )')
+		print('(_/\/\_)(__)(__)(___/ (__) (____)(_)\_)(_/\/\_)(____)(_)\_)(____/ ')
+		print('')
+	
+	
+	
+	def demander_continuer_quitter(self) :
+		entree_valide = False
+		while not entree_valide :
+			print("Commencer le jeu ? (o / n)")
+			entree = input().lower()
+			
+			if entree != 'o' and entree != 'n' :
+				print('Entree invalide')
+				return self
+				
+			if entree == 'o' :
+				return Mastermind_jouer(self.data)
+			
+			if entree == 'n' :
+				self.data.running = False
+				return self
 
 
 
