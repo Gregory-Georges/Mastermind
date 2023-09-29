444ad8ad076d28187bbe003ebd9abc16e5404561
Gregory-Georges <simon.schetagne@gmail.com>
2023-09-28 21:26:56 -0400

    Etat fonctionnel du projet

---
+++ couleur.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+# Valeur associes a des couleurs
+class Couleurs(Enum) :
+	Rouge	= 1
+	Bleu	= 2
+	Jaune	= 3
+	Vert	= 4
+	Mauve	= 5
+	Orange	= 6
--- mastermind.py
+++ mastermind.py
@@ -1,3 +1,5 @@
+from couleur import Couleurs
+
 #
 # Donnees de l'application
 #
@@ -5,6 +7,10 @@ class Mastermind_data:
 	
 	def __init__(self) :
 		self.running = True
+		self.reponse_acceptee = {'rouge': Couleurs.Rouge, 'jaune': Couleurs.Jaune, 'bleu': Couleurs.Bleu, 'vert': Couleurs.Vert, 'mauve': Couleurs.Mauve, 'orange': Couleurs.Orange, 'r': Couleurs.Rouge, 'j': Couleurs.Jaune, 'b': Couleurs.Bleu, 'v':Couleurs.Vert, 'm': Couleurs.Mauve, 'o': Couleurs.Orange}
+		self.combinaison_secrete = (Couleurs.Rouge, Couleurs.Rouge, Couleurs.Rouge, Couleurs.Rouge, Couleurs.Rouge)
+		self.entree_divisee = (0) * 5
+		self.nb_essais = 1
 
 
 
@@ -12,41 +18,84 @@ class Mastermind_data:
 
 
 #
-# Stade de l'applicaton (State machine)
+# Stades de l'applicaton (State machine)
 #
 
 
-# Classe base
+
 class Mastermind_state:
 	
 	def __init__(self, data) :
 		self.data = data
 		
 	def run(self) :
-		pass
+		pass;
+		
+		
+		
+class Mastermind_reussi:
+	
+	def __init__(self, data) :
+		self.data = data
+		
+	def run(self) :
+		print('Vous avez devine la bonne combinaison')
+		return self
 		
 		
 
 # Stade jeu
-class Mastermind_jouer(Mastermind_state):
+class Mastermind_jouer:
 	def __init__(self, data) :
 		Mastermind_state.__init__(self, data)
 		
 	def run(self) :
-		return self
+		self.entrer_combinaison()
+		return self.verifier_entree()
+		
+	def entrer_combinaison(self) :
+		print("entrez une combinaison de 5 couleurs separees d'un espace (ex: b vert r jaune o)")
+		entree = input()
+		self.data.entree_divisee = entree.split(' ')
+				
+	def verifier_entree(self) :
+	
+		# Verifier le nombre de couleurs
+		if(len(self.data.entree_divisee) > 5):
+			print("Trop d'entrees")
+			return self
+		if(len(self.data.entree_divisee) < 5):
+			print("Pas assez d'entrees")
+			return self
+			
+		# Verifier que toutes les entrees sont bonnes
+		for element in self.data.entree_divisee:
+			if element not in self.data.reponse_acceptee:
+				print("Au moins une entree est invalide")
+				return self
+				
+		# Construire la combinaison en nombres
+		combinaison_en_nombres = (self.data.reponse_acceptee[self.data.entree_divisee[0]], self.data.reponse_acceptee[self.data.entree_divisee[1]], self.data.reponse_acceptee[self.data.entree_divisee[2]], self.data.reponse_acceptee[self.data.entree_divisee[3]], self.data.reponse_acceptee[self.data.entree_divisee[4]])
+				
+		# Verifier que la combinaison est bonne
+		if(self.data.combinaison_secrete != combinaison_en_nombres) :
+			self.data.nb_essais = self.data.nb_essais + 1
+			return self
+				
+				
+		return Mastermind_reussi(self.data)
 
 		
 				
 # Stade begin
-class Mastermind_begin(Mastermind_state):
+class Mastermind_begin:
 	
 	def __init__(self, data) :
 		Mastermind_state.__init__(self, data)
 		
 	def run(self) :
 		self.afficher_intro() 
-		state = self.demander_continuer_quitter()
-		return state
+		return self.demander_continuer_ou_quitter()
 		
 	
 	
@@ -59,22 +108,21 @@ class Mastermind_begin(Mastermind_state):
 	
 	
 	
-	def demander_continuer_quitter(self) :
-		entree_valide = False
-		while not entree_valide :
-			print("Commencer le jeu ? (o / n)")
-			entree = input().lower()
+	def demander_continuer_ou_quitter(self) :
+	
+		print("Commencer le jeu ? (o / n)")
+		entree = input().lower()
+		
+		if entree != 'o' and entree != 'n' :
+			print('Entree invalide')
+			return self
 			
-			if entree != 'o' and entree != 'n' :
-				print('Entree invalide')
-				return self
-				
-			if entree == 'o' :
-				return Mastermind_jouer(self.data)
-			
-			if entree == 'n' :
-				self.data.running = False
-				return self
+		if entree == 'o' :
+			return Mastermind_jouer(self.data)
+		
+		if entree == 'n' :
+			self.data.running = False
+			return self
 
 
 
